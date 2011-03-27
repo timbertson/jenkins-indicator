716f93d3ed86afe09d8a0f63d9e7d2c10d05d8c4
James Dunwoody <jamesdunwoody@gmail.com>
2011-03-27 22:59:37 +1100

    Sort of got the play to work

---
+++ src/gtkPlay.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import pygtk
+import sys
+pygtk.require('2.0')
+import gtk
+
+class GtkPlay:
+	def __init__(self):
+		print "init"
+
+	def showDrawingArea(self):
+		print "show drawing area"
+		area = gtk.DrawingArea()
+		area.add_events(gtk.gdk.BUTTON_PRESS_MASK)
+		area.connect("button_pressed_event", self.on_clicked)
+
+	def on_clicked(self):
+		print "clicked"
+
+	def showButton(self):
+		print "showButton"
+		button = gtk.Button()
+		button.set_relief(gtk.RELIEF_NONE)
+		button.set_label("ExampleButton")
+		#button.connect("button_press_event", showMenu, applet)
+		
+	def setupWindow(self):
+		print "setupWindow"
+		mainWindow = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		mainWindow.set_title("GTK Play")
+		mainWindow.connect("destroy", gtk.main_quit)
+		mainWindow.show_all()
+		gtk.main()
+		sys.exit()
+
+if __name__ == '__main__':
+	print "main"
+	Play = GtkPlay()
+	Play.showDrawingArea()
+	Play.setupWindow()
+
