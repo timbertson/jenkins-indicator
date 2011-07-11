53537a4c13a071b6a4317dda4986bb73fbbe9b6c
gfxmonk <tim@gfxmonk.net>
2011-07-11 18:33:15 +1000

    ignore proxy

--- src/job_status_parser.py
+++ src/job_status_parser.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
-import urllib
+import urllib2
 import logging
 import json
 
 class JobStatusParser:
 	def __init__(self, base_uri):
 		self.base_uri = base_uri
+		proxy_support = urllib2.ProxyHandler({})
+		opener = urllib2.build_opener(proxy_support)
+		urllib2.install_opener(opener)
 
 	def parse(self):
-		raw_json = urllib.urlopen(self.base_uri).read()
+		response = urllib2.urlopen(self.base_uri)
+		raw_json = response.read()
 		json_jobs = json.loads(raw_json).get("jobs")
 		return json_jobs
