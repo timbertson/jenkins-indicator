1a85bb4813e0777aa4fb3daa5d52e041eb10455a
Tim <Tim>
2011-07-13 13:05:14 +1000

    add building indicator

--- jenkins_indicator/job.py
+++ jenkins_indicator/job.py
@@ -2,7 +2,7 @@ class Job(object):
 	tick = u'\u2713'
 	cross = u'\u2718'
 	dash  = ' - '
-	good_colors = ['blue', 'blueanime']
+	good_colors = ['blue', 'blue_anime']
 	disabled_colours = ['grey']
 	okay_colors = good_colors + disabled_colours
 
@@ -18,6 +18,9 @@ class Job(object):
 		return "<Job: %r>" % (self.json)
 
 	def is_successful(self):
+		print repr(self.okay_colors)
+		print self.color
+		print self.color in self.okay_colors
 		return self.color in self.okay_colors
 
 	def is_disabled(self):
@@ -30,7 +33,10 @@ class Job(object):
 	def description(self):
 		if(self.is_successful()):
 			prefix = self.dash if self.is_disabled() else self.tick
-			return "%s %s" % (prefix, self.name)
+			desc = "%s %s" % (prefix, self.name)
 		else:
 			return "%s %s (%s)" % (self.cross, self.name, self.color)
+		if self.color.endswith("_anime"):
+			return desc + " (building)"
+		return desc
 
